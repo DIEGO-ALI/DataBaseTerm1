f87a631779927b0c56e7d7606474402b87380979
SantiagoRR17 <santiagorochar.03@gmail.com>
2025-09-01 06:10:45 -0500

    se realiza el contenido de santiago.html, junto con el respectivo css y funciones de javascript

---
+++ functions.js
@@ -0,0 +1,17 @@
+function showArticle1(){
+    document.querySelector('.article1').style.visibility = 'visible';
+}
+
+function showArticle2(){
+    document.querySelector('.article2').style.visibility = 'visible';
+}
+
+function showArticle3(){
+    document.querySelector('.article3').style.visibility = 'visible';
+}
+
+function clearArticles(){
+    document.querySelector('.article1').style.visibility = 'hidden';
+    document.querySelector('.article2').style.visibility = 'hidden';
+    document.querySelector('.article3').style.visibility = 'hidden';
+}
